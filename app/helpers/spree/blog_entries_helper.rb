69dfae39e9afd0bf4ebfe2e55054806e7e88f8f3
ilya-bez <ilya.bezpaly@gmail.com>
2018-04-27 14:11:21 +0300

    fix

--- app/helpers/spree/blog_entries_helper.rb
+++ app/helpers/spree/blog_entries_helper.rb
@@ -6,27 +6,28 @@ module Spree
     end
 
     def blog_entry_permalink(e)
-      unless e.published_at.nil?
-        blog_entry_permalink_path :year => e.published_at.strftime("%Y"), :month => e.published_at.strftime("%m"), :day => e.published_at.strftime("%d"), :slug => e.permalink
-      else
-        blog_entry_permalink_path :year => "na", :month => "na", :day => "na", :slug => e.permalink
-      end
+      blog_entry_permalink_path :slug => e.permalink
+      #unless e.published_at.nil?
+      #  blog_entry_permalink_path :slug => e.permalink
+      # else
+      #  blog_entry_permalink_path :year => "na", :month => "na", :day => "na", :slug => e.permalink
+      #end
     end
 
-    def blog_entry_url_permalink(e)
-      unless e.published_at.nil?
-         blog_entry_permalink_url :year => e.published_at.strftime("%Y"), :month => e.published_at.strftime("%m"), :day => e.published_at.strftime("%d"), :slug => e.permalink
-       else
-        blog_entry_permalink_url :year => "na", :month => "na", :day => "na", :slug => e.permalink
-      end
-    end
+    #def blog_entry_url_permalink(e)
+      #unless e.published_at.nil?
+      #   blog_entry_permalink_url :year => e.published_at.strftime("%Y"), :month => e.published_at.strftime("%m"), :day => e.published_at.strftime("%d"), :slug => e.permalink
+      # else
+      #  blog_entry_permalink_url :year => "na", :month => "na", :day => "na", :slug => e.permalink
+      #end
+    #end
 
     def blog_full_article_html(blog_entry)
-      "<br><br>Read the full article #{link_to blog_entry.title, blog_entry_url_permalink(blog_entry)} at #{link_to "#{Spree::Config[:site_name]} Blog", blog_url}."
+      "<br><br>Read the full article #{link_to blog_entry.title, blog_entry_permalink(blog_entry)} at #{link_to "#{Spree::Config[:site_name]} Blog", blog_url}."
     end
 
     def blog_first_appeared_html(blog_entry)
-      "<br><br>The article #{link_to blog_entry.title, blog_entry_url_permalink(blog_entry)} first appeared on #{link_to "#{Spree::Config[:site_name]} Blog", blog_url}."
+      "<br><br>The article #{link_to blog_entry.title, blog_entry_permalink(blog_entry)} first appeared on #{link_to "#{Spree::Config[:site_name]} Blog", blog_url}."
     end
 
     def blog_entry_tag_list_html blog_entry
--- app/views/spree/blog_entries/feed.rss.builder
+++ app/views/spree/blog_entries/feed.rss.builder
@@ -11,8 +11,8 @@ xml.rss :version => "2.0" do
         xml.description blog_entry.entry_summary + blog_full_article_html(blog_entry)
         xml.content blog_entry.body + blog_first_appeared_html(blog_entry), :type => :html
         xml.pubDate blog_entry.published_at.to_s(:rfc822)
-        xml.link blog_entry_url_permalink(blog_entry)
-        xml.guid blog_entry_url_permalink(blog_entry)
+        xml.link blog_entry_permalink(blog_entry)
+        xml.guid blog_entry_permalink(blog_entry)
         blog_entry.tag_list.each do |tag|
           xml.category tag
         end
